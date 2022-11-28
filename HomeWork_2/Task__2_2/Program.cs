1f64028d138f9cb0e41f5fcb156dcbf71558a617
AX400 <>
2022-11-28 17:02:05 +0300

    add Seminar_2__Homework

---
+++ HomeWork_2/Task__2_2/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Write("Введите число:");
+int num = int.Parse(Console.ReadLine());
+int count = num.ToString().Length;
+int k = 1;
+int result = 0;
+
+if (count < 3)
+    Console.WriteLine($"Число {num} - третьей цифры нет.");
+else {
+    while(count > 3)
+    {
+        k = k * 10;
+        count = count - 1;
+    }
+    result = (num / k) % 10;
+    Console.WriteLine($"{num}: {result}");
+}
+
---
+++ HomeWork_2/Task__2_3/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Write("Введите цифру дня недели: ");
+int num = int.Parse(Console.ReadLine());
+
+
+switch (num)
+{
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5: 
+            Console.Write($"{num}: Будний день");
+            break;
+    case 6:
+    case 7:
+            Console.Write($"{num}: ВЫХОДНОЙ!");
+            break;
+    default: 
+            Console.WriteLine("В неделе 7 дней!");
+            break;
+}
